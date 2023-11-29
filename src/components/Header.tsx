88d1c973f9e050f370cb19c29787bc446e010542
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2023-11-29 18:06:05 +0100

    Add @headlessui/react, @heroicons/react and refactor Header component
    Added the @headlessui/react and @heroicons/react dependencies to improve accessibility and UX on the project. Simultaneously, the Header.tsx component has been refactored to use these new libraries providing a better mobile navigation system, and an updated gradient color scheme was implemented for consistency across devices.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
+// Importing necessary modules, components and hooks from respective packages
 "use client";
+import { Disclosure } from "@headlessui/react"; // Used for creating accessible components
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"; // Icons from Heroicons
+import React from "react";
+import Link from "next/link"; // Navigation between pages in Next.js application
+import { usePathname } from "next/navigation"; // Hook provided by Next.js for accessing current URL pathname
 
-import React, { FC, useState } from "react";
-import Link from "next/link";
-
-// Define the shape of the object for link properties.
+// Define the properties for links
 interface LinkProps {
   href: string;
   label: string;
 }
 
-// Links array holding paths and labels for navigation.
+// Array of links for the application
 const links: LinkProps[] = [
   { href: "/", label: "Home" },
   { href: "/performance", label: "Performance" },
@@ -17,45 +20,63 @@ const links: LinkProps[] = [
   { href: "/scale", label: "Scale" },
 ];
 
-// Header component containing navigation links.
-const Header: FC = () => {
-  // State to handle opening and closing of the mobile menu
-  const [isOpen, setOpen] = useState(false);
+// Header component definition
+export default function Header() {
+  // Use Next.js provided hook to get the current pathname
+  const pathname = usePathname();
 
+  // Return JSX for the component
   return (
-    <nav className="relative flex items-center justify-between px-8 py-4 bg-gradient-to-r from-gray-900 to-gray-700">
-      <div className="text-2xl font-bold text-white">Logo</div>
-      <div className="hidden md:flex space-x-8">
-        {links.map(({ href, label }) => (
-          <Link key={href} href={href}>
-            <span className="py-2 block text-white cursor-pointer">
-              {label}
-            </span>
-          </Link>
-        ))}
-      </div>
-      <button
-        className="md:hidden rounded-lg text-white focus:outline-none focus:shadow-outline"
-        onClick={() => setOpen(!isOpen)}
-      >
-        {isOpen ? "Close" : "Menu"}
-      </button>
-      {isOpen && (
-        <div className="absolute top-full left-0 w-full bg-gradient-to-r from-gray-900 to-gray-700 md:hidden">
-          <div className="p-4 space-y-4 text-right">
-            {links.map(({ href, label }) => (
-              <Link key={href} href={href}>
-                <span className="py-2 block text-white cursor-pointer">
-                  {label}
-                </span>
-              </Link>
-            ))}
+    // A navigation bar with a gradient background
+    <Disclosure as="nav" className="bg-gradient-to-r from-gray-900 to-gray-800">
+      {({ open }) => (
+        <>
+          <div className="flex justify-between px-8 py-4">
+            <div className="text-2xl font-bold text-white">Logo</div>
+            <div className="hidden md:flex space-x-8">
+              {/* Mapping through the links array to display the links */}
+              {links.map(({ href, label }) => (
+                <Link key={href} href={href}>
+                  {/* Highlight the active link */}
+                  <span
+                    className={`py-2 text-white cursor-pointer ${
+                      pathname === href ? "underline" : ""
+                    }`}
+                  >
+                    {label}
+                  </span>
+                </Link>
+              ))}
+            </div>
+            {/* Toggling button for displaying the menu on smaller screens */}
+            <Disclosure.Button className="md:hidden rounded-lg text-white focus:outline-none focus:shadow-outline">
+              {/* Changing icon depending on whether the menu is open */}
+              {open ? (
+                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Disclosure.Button>
           </div>
-        </div>
+          {/* Panel for displaying the menu on smaller screens */}
+          <Disclosure.Panel className="md:hidden">
+            <div className="p-4 space-y-4 bg-gradient-to-r from-gray-900 to-gray-800">
+              {links.map(({ href, label }) => (
+                <Link key={href} href={href}>
+                  {/* Highlight the active link */}
+                  <span
+                    className={`block text-white py-2 cursor-pointer ${
+                      pathname === href ? "underline" : ""
+                    }`}
+                  >
+                    {label}
+                  </span>
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
       )}
-    </nav>
+    </Disclosure>
   );
-};
-
-// Export the Header component
-export default Header;
+}
